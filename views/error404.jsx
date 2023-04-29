acc78f7c6ba16099808d49b6b31a649dc20ed9d3
Brenda <amaya.brendaa@gmail.com>
2023-04-28 21:33:59 -0700

    Adding 404 page view

---
+++ views/error404.jsx
@@ -0,0 +1,16 @@
+const React = require('react')
+const Def = require('./default')
+
+function error404 () {
+    return(
+    <Def>
+        <main>
+            <h1> 404: PAGE NOT FOUND</h1>
+            <p>Oops, sorry, we cannot find this page!</p>
+            <img src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw4wMuLUjH4mmiAsTxEEUISPhPtoN_UZ-8Hg&usqp=CAU' alt='ghost saying page not found'></img>
+        </main>
+    </Def>
+    )
+}
+
+module.exports = error404
