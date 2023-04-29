3ea3a61da6dd4541d6c9946aabcdf9834240cb32
Brenda <amaya.brendaa@gmail.com>
2023-04-28 21:43:44 -0700

    made places folder

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
 
-router.get('/', (res,req) => {
+/* router.get('/', (res,req) => {
     res.setEncoding('GET /places')
-})
+})  */
 
+//GET /places
+router.get('/' , (req,res) => {
+    res.render('places/index')
+})
 module.exports = router
