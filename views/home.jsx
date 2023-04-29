3fee1de78b73643dbff63b07a8a8570bd7cda06f
Brenda <amaya.brendaa@gmail.com>
2023-04-28 22:46:50 -0700

    adding home image

--- views/home.jsx
+++ views/home.jsx
@@ -9,6 +9,12 @@ function home() {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img width='480' src='/images/libra.jpeg' alt='libra symbol'></img>
+                    <div>
+                        Photo by <a href='https://www.vecteezy.com/members/screamsilence88291710' target='_blank' >Silence Scream</a> on <a href='https://www.vecteezy.com/vector-art/4679953-illustration-vector-antique-libra-scales-ornament-monochrome-style' target='_blank' > Vecteezy</a>
+                    </div>
+                </div>
                 <a href='/places'>
                     <button className='btn-primary'>Places Page</button>
                 </a>
