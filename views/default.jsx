9d02ddf747d2fa02952239908e5efa73fea45e16
Brenda <amaya.brendaa@gmail.com>
2023-04-30 21:00:24 -0700

    added nav bar & some style

--- views/default.jsx
+++ views/default.jsx
@@ -11,6 +11,19 @@ function Def (html) {
                 <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'></link>
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href='/'>Home</a>
+                        </li>
+                        <li>
+                            <a href='/places'>Places</a>
+                        </li>
+                        <li>
+                        <a href='/places/new'>Add A New Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body> 
         </html>
