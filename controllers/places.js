29b5e92cd7f1172efbbf1a88994180fa34ed5fb8
Brenda <amaya.brendaa@gmail.com>
2023-05-05 16:31:07 -0700

    added error message on page for dates

--- controllers/places.js
+++ controllers/places.js
@@ -131,7 +131,15 @@ router.post('/', (req, res) => {
         res.redirect('/places')
     })
     .catch(err => {
-        console.log('error', err)
+        if (err && err.name == 'ValidationError') {
+            let message = 'Validation Error:'
+            for (var field in err.errors) {
+                message += ` ${field} was ${err.errors[field].value}. `
+                message += `${err.errors[field].message}`
+            }
+            console.log('Validation error message', message)
+            res.render('places/new', { message })
+        }
         res.render('error404')
     })
 })
