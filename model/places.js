dba2d368a878cd9cdcd1d3bcb92443c996e75113
Brenda <amaya.brendaa@gmail.com>
2023-05-08 22:26:04 -0700

    added comment to database

--- model/places.js
+++ model/places.js
@@ -8,7 +8,12 @@ const placeSchema = new Schema({
     cuisines: {type: String, required: true },
     city: {type: String, default: 'Anytown'},
     state: {type: String, default: 'USA'},
-    founded: {type: Number, min:[1673, 'Surely not that old?!'], max:[new Date().getFullYear(), 'Hey, this year is in the future!']}
+    founded: {
+        type: Number, 
+        min:[1673, 'Surely not that old?!'], 
+        max:[new Date().getFullYear(), 'Hey, this year is in the future!']
+    },
+    comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
 })
 
 placeSchema.methods.showEstablished = function() {
---
+++ seeders/seed-comments.js
@@ -0,0 +1,26 @@
+const db = require('../model')
+
+//TO USE AWAIT, WE NEED AN ASYNC FUNCTION
+async function seed(){
+    //Get the place
+    let place = await db.Place.findOne({ name: '444' })
+
+    //Create a fake sample comment
+    let comment = await db.Comment.create({
+        author: 'Beast Ernie',
+        rant: false,
+        stars: 5.0,
+        content: 'Wow, simply amazing! Highly recommended'
+    })
+
+    //Add that comment to the place's comment array
+    place.comments.push(comment.id)
+
+    //Save the place now that it has a comment
+    await place.save()
+
+    //Exit the program
+    process.exit()
+}   
+
+seed() 
